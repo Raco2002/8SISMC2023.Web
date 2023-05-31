85225899cd93f5f8113404648e2a8734cfd16f56
Raco2002 <lr.mota.c@edu.utc.mx>
2023-05-30 22:41:34 -0600

    Avance de tarea e interfaz

---
+++ Tareas/css problemas algor/validaprob.js
@@ -0,0 +1,11 @@
+function validar(problemas) {
+    var tasaInteres = 0.02; // Tasa de interés mensual (2%)
+    var tiempo = 1; // Número de meses
+    var montoGastado = capital * Math.pow(1 + tasaInteres, tiempo); // Cálculo del monto gastado después de un mes
+    
+    return montoGastado; // Devuelve el monto gastado después de un mes.
+
+        if(montoGastado = 5){
+        alert("Ingrese otro numero");
+    }
+  }
