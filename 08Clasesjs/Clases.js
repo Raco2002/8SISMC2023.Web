a8b6a724d666f52cc6e7befdb86a1e6c5081e63a
Raco2002 <lr.mota.c@edu.utc.mx>
2023-06-21 13:18:08 -0600

    ejemplo del dom

--- 08Clasesjs/Clases.js
+++ 08Clasesjs/Clases.js
@@ -1,30 +1,30 @@
 /*
-    Como habiamos dicho, js es multiparadigma
-    en este caso vamos a ver como seria manejar
-    js en poo (programacion orientada a objetos).
-*/
+Como habiamos dicho js es multiparadigma
+en este caso vamos a ver como seria manejar
+js en poo
+*/ 
 
-class figuraGeometrica{
+class FiguraGeometrica{
+
     //constructor
-    constructor()
-    //puede o no tener alguna implementacion
+    constructor(){
+        //puede o no tener alguna implementacion
+    }
 
+    //metodos
+    area(){
+        //metodo que se encarga de calcular el area
+    }
 
-//metodos
-area(){
-    //metodo que se encarga de calcular el area
+    perimetro(){
+        //metodo para el calculo de perimetros
+        console.log("Este metodo calcula el perimetro");
+    }
 }
 
-perimetro(){
-    //metodo para el calculo de perimetros
-    console.log("Este metodo calcula el perimetro")
-}
-
-}
-
-class Rectangulo extends figuraGeometrica{
+class Rectangulo extends FiguraGeometrica{
     //constructor
-    constructor(base,altura){
+    constructor(base, altura){
         super();
         this._base = base;
         this._altura = altura;
@@ -32,6 +32,20 @@ class Rectangulo extends figuraGeometrica{
         this._perimetro = null;
         this._actualizarArea = false;
         this._actualizarPerimetro = false;
+    }
+
+    set altura(altura){
+        this._altura = altura;
+        //si cambia el valor del area o perimetro hay que actualizar el dato
+        this._actualizarArea = true;
+        this._actualizarPerimetro = true;
+    }
+
+    set base(base){
+        this._base = base;
+        //si cambia el valor del area o perimetro hay que actualizar el dato
+        this._actualizarArea = true;
+        this._actualizarPerimetro = true;
     }
 
     get area(){
@@ -51,13 +65,13 @@ class Rectangulo extends figuraGeometrica{
     calcularArea(){
         console.log(this._base);
         console.log(this._altura);
-        return this._base * this._altura
+        return this._base * this._altura;
     }
 
     calcularPerimetro(){
         console.log(this._base);
         console.log(this._altura);
-        return this._base + this._altura
+        return this._base + this._altura;
     }
 }
 
@@ -67,38 +81,41 @@ console.log(objetoRectangulo.calcularArea());
 console.log(objetoRectangulo.calcularPerimetro());
 
 
+
 //Spread
 /*
-Es una sintaxis que nos permite a un elemneto iterable (arreglo, matriz, vector, cadena)
-poder desde cero contar los argmnetos que pasan por sicho elemento
+Es una sintaxis que nos permite a un elemento iterable
+(arreglo, matriz, vector, cadena) poder desde 0 contar
+los argumentos que pasan por dicho elemento
 */
 
 //tenemos el siguiente arreglo
-const arregloOrdenadoMayorMenor = [10, 9 ,8, 7, 6 , 5, 4, 3, 2, 1, 0];
-console.log(`Arreglo ordenado: ${arregloOrdenadoMayorMenor}`);
+const arregloOrdenadoMayorMenor = [10, 9 , 8 , 7 , 6, 5 ,4,3,2,1,0];
+
+console.log(`Arreglo ordenado:  ${arregloOrdenadoMayorMenor} `);
 
 //vamos a suponer que podemos obtener tantas variables del arreglo como deseemos a partir del patron
 
 const[valorMasGrande] = arregloOrdenadoMayorMenor;
-console.log (`Valor mas Grande: ${valorMasGrande}`);
+console.log(`Valor mas Grande: ${valorMasGrande}` );
 
 //vamos a obtener el resto de los valores
 
 const[valorMasGrande1, valorMasGrande2, valorMasGrande3, ...restoValores] = arregloOrdenadoMayorMenor;
 
-console.log(`VMG1, VMG2, VMG3, ...Resto :  ${valorMasGrande1, valorMasGrande2, valorMasGrande3}, ${restoValores}`);
+console.log(`VMG1, VMG2, VMG3, ...Resto : ${valorMasGrande1}, ${valorMasGrande2}, ${valorMasGrande3}, ${restoValores}`);
 
 //ejemplo de una busqueda simplificada
 
 const resultadoDeBusqueda = {
     resultados: [
-        "resultado 1",
-        "resultado 2",
-        "resultado 3",
-        "resultado 4",
-        "resultado 5",
-        "resultado 6",
-        "resultado 7",
+        "resultado1",
+        "resultado2",
+        "resultado3",
+        "resultado4",
+        "resultado5",
+        "resultado6",
+        "resultado7"
     ],
     total: 7,
     mejorCoincidencia : "resultado3"
@@ -112,17 +129,14 @@ const {mejorCoincidencia} = resultadoDeBusqueda;
 
 console.log(`Mejor coincidencia : ${mejorCoincidencia}`);
 
-/* 
-Vamos a suponer que queremos cambiar el nombre, derivado a que necesitamos mantener
-la persistencia del codigo acorde a una nomenclatura
-*/
+//vamos a suponer que queremos cambiar el nombre, derivado a que necesitamos mantener la persistencia del codigo acorde a una nomenclatura
 
-const copiaResultadoDeBusqueda = {... resultadoDeBusqueda};
+const copiaResultadoDeBusqueda = {...resultadoDeBusqueda};
 
 console.log(`Copia del resultado de busqueda : ${copiaResultadoDeBusqueda}`);
 
 //vamos a modificarlo
 
-const copiaResultadoDeBusquedaModificar = {... resultadoDeBusqueda, cadenaBuscada : "resultado 3"};
+const copiaResultadoDeBusquedaModificar = {...resultadoDeBusqueda, cadenaBuscada : "resultado3"};
 
-console.log(`Copia modificada : ${copiaResultadoDeBusquedaModificar}`);
+console.log(`Copia modificada :  ${copiaResultadoDeBusquedaModificar}` );
